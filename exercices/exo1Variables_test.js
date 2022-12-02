a9228ffa3385bd6253657c7b6c9b4d061d78c72c
RenaudC-LADAPT <72964373+RenaudC-LADAPT@users.noreply.github.com>
2022-12-02 10:08:46 +0100

    Create exo1Variables_test.js

---
+++ exercices/exo1Variables_test.js
@@ -0,0 +1,26 @@
+﻿﻿var assert = require('assert');
+var mod = require('./exo1Variables.js');
+
+it("Création des variables : num, txt, bin et flt", function () {
+  try
+  {
+    assert.equal( 42, mod.num );
+    assert.equal( typeof(mod.num), "number" );
+    assert.equal( "42", mod.txt );
+    assert.equal( typeof(mod.txt), "string" );
+    assert.equal( true, mod.bin );
+    assert.equal( typeof(mod.bin), "boolean" );
+    assert.equal( 0.42, mod.flt );
+    assert.equal( typeof(mod.flt), "number" );
+  }
+  catch (error)
+  {
+    printMessage('Conseil 💡', "Revoyez la déclaration des variables 🤔");
+    throw error;
+  }
+});
+
+function printMessage(channel, message)
+{
+  console.log('\nTECHIO> message --channel "' + channel + '" "' + message + '"');
+}
