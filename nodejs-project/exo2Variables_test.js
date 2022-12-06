7a09666a63b6c93dd6b041bfda1a2f4712e8ad3d
RenaudC-LADAPT <72964373+RenaudC-LADAPT@users.noreply.github.com>
2022-12-06 16:16:05 +0100

    Update exo2Variables_test.js

--- nodejs-project/exo2Variables_test.js
+++ nodejs-project/exo2Variables_test.js
@@ -2,9 +2,9 @@ var assert = require('assert');
 var mod = require('./exo2Variables.js');
 
 it("afficher le type des variables : num, str, bool et flt", function () {
+  printMessage('Vos réponses :', "\n" + "la variable num est de type " + mod.numType + "\n" + "la variable str est de type " + mod.strType + "\n" + "la variable bool est de type " + mod.boolType + "\n" + "la variable flt est de type " + mod.fltType);
   try
   {
-    
     assert.equal( "number", mod.numType );
     assert.equal( "string", mod.strType );
     assert.equal(  "boolean", mod.boolType );
@@ -14,10 +14,6 @@ it("afficher le type des variables : num, str, bool et flt", function () {
   {
     printMessage('Conseil 💡', "Revoyez l'utlisation de typeof 🤔");
     throw error;
-  }
-  finally
-  {
-  printMessage('Vos réponses :', "\n" + "la variable num est de type " + mod.numType + "\n" + "la variable str est de type " + mod.strType + "\n" + "la variable bool est de type " + mod.boolType + "\n" + "la variable flt est de type " + mod.fltType);
   }
 });
 
