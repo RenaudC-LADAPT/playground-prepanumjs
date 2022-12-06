c3082ad02db5c0bb57e29ddd7c9d3ec19aa0f543
RenaudC-LADAPT <72964373+RenaudC-LADAPT@users.noreply.github.com>
2022-12-06 15:14:33 +0100

    Update exo3Variables_test.js

--- nodejs-project/exo3Variables_test.js
+++ nodejs-project/exo3Variables_test.js
@@ -4,10 +4,14 @@ var mod = require('./exo3Variables.js');
 it("afficher le type des variables : num, str, bool et flt", function () {
   try
   {
-    assert.equal( "number", mod.numType );
-    assert.equal( "string", mod.strType );
-    assert.equal(  "boolean", mod.boolType );
-    assert.equal( "number", mod.fltType );
+    assert.equal( 42, mod.num );
+    assert.equal( typeof(mod.num), "number" );
+    assert.equal( "42", mod.str );
+    assert.equal( typeof(mod.str), "string" );
+    assert.equal( true, mod.bool );
+    assert.equal( typeof(mod.bool), "boolean" );
+    assert.equal( 0.42, mod.flt );
+    assert.equal( typeof(mod.flt), "number" );
   }
   catch (error)
   {
