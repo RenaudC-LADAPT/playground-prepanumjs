3a54957822d475eb7f8b748f1c4d39201ca9caa0
RenaudC-LADAPT <72964373+RenaudC-LADAPT@users.noreply.github.com>
2022-12-06 16:28:40 +0100

    Update exo2Variables_test.js

--- nodejs-project/exo2Variables_test.js
+++ nodejs-project/exo2Variables_test.js
@@ -2,7 +2,7 @@ var assert = require('assert');
 var mod = require('./exo2Variables.js');
 
 it("afficher le type des variables : num, str, bool et flt", function () {
-  printMessage('Vos réponses :', '\t' + 'la variable num est de type ' + mod.numType + '\t' + 'la variable str est de type ' + mod.strType + '\t' + 'la variable bool est de type ' + mod.boolType + '\t' + 'la variable flt est de type ' + mod.fltType);
+  printMessage('Vos réponses :','la variable num est de type ' + mod.numType + ' / la variable str est de type ' + mod.strType + ' / la variable bool est de type ' + mod.boolType + ' / la variable flt est de type ' + mod.fltType);
   try
   {
     assert.equal( "number", mod.numType );
