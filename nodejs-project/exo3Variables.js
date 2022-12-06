631fa890ed487ca1e596f0d6db86d398ea696ac3
RenaudC-LADAPT <72964373+RenaudC-LADAPT@users.noreply.github.com>
2022-12-06 15:13:47 +0100

    Update exo3Variables.js

--- nodejs-project/exo3Variables.js
+++ nodejs-project/exo3Variables.js
@@ -1,1 +1,15 @@
+let num = 42;
+let str = "42";
+let bool = true;
+let flt = 0.42;
+//compléter le code ci-dessous
+
+// { autofold
+module.exports = {
+	num: num,
+	str: srt,
+	bool: bool,
+	flt: flt	
+};
+// }
 
