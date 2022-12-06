a191f7c650286ead45ca26fcd5cb1c5d03c00d68
RenaudC-LADAPT <72964373+RenaudC-LADAPT@users.noreply.github.com>
2022-12-06 13:26:28 +0100

    Create exo2Variables.js

---
+++ nodejs-project/exo2Variables.js
@@ -0,0 +1,10 @@
+let num = 42;
+let str = "42";
+let bool = true;
+let flt = 0.42;
+//compléter l'argument des console.log ci dessous
+console log("la variable num est de type " + );
+console log("la variable str est de type " + );
+console log("la variable bool est de type " + );
+console log("la variable flt est de type " + );
+            
