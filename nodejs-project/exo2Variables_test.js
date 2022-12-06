137adadfdd388201c949bb408a475153a49afcb8
RenaudC-LADAPT <72964373+RenaudC-LADAPT@users.noreply.github.com>
2022-12-06 13:28:50 +0100

    Create exo2Variables_test.js

---
+++ nodejs-project/exo2Variables_test.js
@@ -0,0 +1,19 @@
+var assert = require('assert');
+var mod = require('./exo1Variables.js');
+
+it("afficher le type des variables : num, str, bool et flt", function () {
+  try
+  {
+    
+  }
+  catch (error)
+  {
+    printMessage('Conseil 💡', "Revoyez l'utlisation de typeof 🤔");
+    throw error;
+  }
+});
+
+function printMessage(channel, message)
+{
+  console.log('\nTECHIO> message --channel "' + channel + '" "' + message + '"');
+}
