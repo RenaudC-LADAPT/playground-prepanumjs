54321dd366683d021da0371c91071fcd210f49e2
RenaudC-LADAPT <72964373+RenaudC-LADAPT@users.noreply.github.com>
2022-12-06 14:57:00 +0100

    Update exo2Variables.js

--- nodejs-project/exo2Variables.js
+++ nodejs-project/exo2Variables.js
@@ -2,17 +2,22 @@ let num = 42;
 let str = "42";
 let bool = true;
 let flt = 0.42;
-//compléter l'argument des console.log ci dessous
-console.log("la variable num est de type " + );
-console.log("la variable str est de type " + );
-console.log("la variable bool est de type " + );
-console.log("la variable flt est de type " + );
+//compléter le code ci-dessous
+let numType = /*à compléter*/;
+let strType = /*à compléter*/;
+let boolType = /*à compléter*/;
+let fltType = /*à compléter*/;
+
+console.log("la variable num est de type " + numType);
+console.log("la variable str est de type " + strType);
+console.log("la variable bool est de type " + boolType);
+console.log("la variable flt est de type " + fltType);
             
 // { autofold
 module.exports = {
-	num: num,
-	str: str,
-	bool: bool,
-	flt: flt	
+	numType: num,
+	numType: str,
+	numType: bool,
+	numType: flt	
 };
 // }
