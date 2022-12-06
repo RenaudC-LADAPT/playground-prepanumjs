9aba2e24da3a127318545aeac6f740af54f61f45
RenaudC-LADAPT <72964373+RenaudC-LADAPT@users.noreply.github.com>
2022-12-06 15:29:12 +0100

    Update exo3Variables_test.js

--- nodejs-project/exo3Variables_test.js
+++ nodejs-project/exo3Variables_test.js
@@ -4,18 +4,18 @@ var mod = require('./exo3Variables.js');
 it("afficher le type des variables : num, str, bool et flt", function () {
   try
   {
-    assert.equal( 42, mod.num );
+    assert.equal( 43, mod.num );
     assert.equal( typeof(mod.num), "number" );
-    assert.equal( "42", mod.str );
+    assert.equal( "42 est la réponse à la grande question sur la vie, l’univers et le reste.", mod.str );
     assert.equal( typeof(mod.str), "string" );
-    assert.equal( true, mod.bool );
+    assert.equal( false, mod.bool );
     assert.equal( typeof(mod.bool), "boolean" );
-    assert.equal( 0.42, mod.flt );
+    assert.equal( 43.42, mod.flt );
     assert.equal( typeof(mod.flt), "number" );
   }
   catch (error)
   {
-    printMessage('Conseil 💡', "Revoyez l'utlisation de typeof 🤔");
+    printMessage('Conseil 💡', "revoyez les opérations de bases en JavaScript 🤔");
     throw error;
   }
 });
