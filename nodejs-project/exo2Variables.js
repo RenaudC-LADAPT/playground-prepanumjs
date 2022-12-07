cf191d2476520d5d9207430ebcd7d725e8ec3854
RenaudC-LADAPT <72964373+RenaudC-LADAPT@users.noreply.github.com>
2022-12-07 11:33:55 +0100

    Update exo2Variables.js

--- nodejs-project/exo2Variables.js
+++ nodejs-project/exo2Variables.js
@@ -3,12 +3,14 @@ let str = "42";
 let bool = true;
 let flt = 0.42;
 let array = [num, str, bool, flt];
-//compléter le code ci-dessous
+let x;
+//compléter le code ci-dessous (utiliser la fonction typeof)
 let numType = /*à compléter*/;
 let strType = /*à compléter*/;
 let boolType = /*à compléter*/;
 let fltType = /*à compléter*/;
 let arrayType = /*à compléter*/;
+let xType =/*à compléter*/;
 
 // { autofold
 module.exports = {
@@ -17,5 +19,6 @@ module.exports = {
 	boolType: boolType,
 	fltType: fltType,
 	arrayType: arrayType,
+	xType: xType
 };
 // }
