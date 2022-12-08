3b00e0cc12af10a11a3a662f1b96e276fca4e74c
RenaudC-LADAPT <72964373+RenaudC-LADAPT@users.noreply.github.com>
2022-12-08 16:11:14 +0100

    Update exo1Fonctions.js

--- nodejs-project/exo1Fonctions.js
+++ nodejs-project/exo1Fonctions.js
@@ -1,1 +1,15 @@
+// Executez ce code plusieurs fois en modifiant la valeur de la variable num
+let num = 4 ; // modifiez la valeur de num
+
+function calcul (x) { 
+	let y; 
+	y=3*x+2; 
+	return y; 
+} 
+console.log("y=3x+2 avec x = " + num + " Résultat : y = " + calcul(num));
+
+// { autofold
+module
+
+// }
 
