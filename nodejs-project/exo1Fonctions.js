7f633f937e24523546bce5aa9c6b7dd66a039589
RenaudC-LADAPT <72964373+RenaudC-LADAPT@users.noreply.github.com>
2022-12-08 16:25:43 +0100

    Update exo1Fonctions.js

--- nodejs-project/exo1Fonctions.js
+++ nodejs-project/exo1Fonctions.js
@@ -1,20 +1,13 @@
 // Executez ce code plusieurs fois en modifiant la valeur de la variable num
 let num = 4 ; // modifiez la valeur de num
 
-function afficheNom (nom) { 
-	console.log("Bonjour ", nom);
-	//ecrit "Bonjour nom" où nom est une String donné en argument de la fonction
-} 
-let nom_utilisateur = prompt("Quel est votre nom ?"); 
-afficheNom(nom_utilisateur);
-
-/*function calcul (x) { 
+function calcul (x) { 
 	let y; 
 	y=3*x+2; 
 	return y; 
 } 
 console.log("y=3x+2 avec x = " + num + " Résultat : y = " + calcul(num));
-*/
+
 // { autofold
 module.exports = {
 	num: num,
