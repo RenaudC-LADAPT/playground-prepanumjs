124824dad49f1a4d99caa0de49dc4ac06de5dfde
RenaudC-LADAPT <72964373+RenaudC-LADAPT@users.noreply.github.com>
2022-12-13 11:25:00 +0100

    Update exo4Fonctions.js

--- nodejs-project/exo4Fonctions.js
+++ nodejs-project/exo4Fonctions.js
@@ -1,1 +1,13 @@
+//compléter le code ici afin de déclarer une fonction nommée getSurface qui prend la base et la hauteur d’un triangle et retourne sa surface.
 
+
+
+
+// { autofold
+let test1 = getSurface(8, 2);
+let test2 = getSurface(7, 3);
+module.exports = {
+	test1: test1,
+	test2: test2,
+};
+// }
