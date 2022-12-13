45b2a1020f7906dd53baafd68162519f5fed3ab5
RenaudC-LADAPT <72964373+RenaudC-LADAPT@users.noreply.github.com>
2022-12-13 11:16:25 +0100

    Update exo3Fonctions_test.js

--- nodejs-project/exo3Fonctions_test.js
+++ nodejs-project/exo3Fonctions_test.js
@@ -1,1 +1,22 @@
+var assert = require('assert');
+var mod = require('./exo3Fonctions.js');
 
+it("Minutes en secondes", function () {
+  try
+  {
+    assert.equal( typeof(mod.test1), "number" );
+    assert.equal( mod.test1, 240 );
+    assert.equal( typeof(mod.test2), "number" );
+     assert.equal( mod.test1, 180 );
+  }
+  catch (error)
+  {
+    printMessage('Attention', 'revoyez la déclaration de votre fonction minuteToSecond');
+    throw error;
+  }
+});
+
+function printMessage(channel, message)
+{
+  console.log('\nTECHIO> message --channel "' + channel + '" "' + message + '"');
+}
