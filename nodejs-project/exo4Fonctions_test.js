60224b373246ebb51dbdb2a84e50d6f81cb6947d
RenaudC-LADAPT <72964373+RenaudC-LADAPT@users.noreply.github.com>
2022-12-13 11:22:58 +0100

    Update exo4Fonctions_test.js

--- nodejs-project/exo4Fonctions_test.js
+++ nodejs-project/exo4Fonctions_test.js
@@ -1,1 +1,23 @@
 
+var assert = require('assert');
+var mod = require('./exo4Fonctions.js');
+
+it("Aire du triangle", function () {
+  try
+  {
+    assert.equal( typeof(mod.test1), "number" );
+    assert.equal( mod.test1, 8 );
+    assert.equal( typeof(mod.test2), "number" );
+     assert.equal( mod.test2, 10.5 );
+  }
+  catch (error)
+  {
+    printMessage('Attention', 'revoyez la déclaration de votre fonction minuteToSecond');
+    throw error;
+  }
+});
+
+function printMessage(channel, message)
+{
+  console.log('\nTECHIO> message --channel "' + channel + '" "' + message + '"');
+}
