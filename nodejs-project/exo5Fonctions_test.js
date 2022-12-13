67e93bd4e6406f7b6dbdb2962793548d47bb30e1
RenaudC-LADAPT <72964373+RenaudC-LADAPT@users.noreply.github.com>
2022-12-13 11:57:38 +0100

    Update exo5Fonctions_test.js

--- nodejs-project/exo5Fonctions_test.js
+++ nodejs-project/exo5Fonctions_test.js
@@ -1,1 +1,29 @@
 
+var assert = require('assert');
+var mod = require('./exo5Fonctions.js');
+
+it("Nombre d'Oeufs", function () {
+  try
+  {
+    assert.equal( typeof(mod.test1), "number" );
+    assert.equal( mod.test1, 4 );
+    assert.equal( typeof(mod.test2), "number" );
+    assert.equal( mod.test2, 6 );
+    assert.equal( typeof(mod.test3), "number" );
+    assert.equal( typeof(mod.test3b), "number" );
+    assert.equal( mod.test3, mod.test3b);
+    assert.equal( typeof(mod.test4), "number" );
+    assert.equal( typeof(mod.test4b), "number" );
+    assert.equal( mod.test4, mod.test4b );
+  }
+  catch (error)
+  {
+    printMessage('Attention', 'revoyez la déclaration de votre fonction nbrsOeufs');
+    throw error;
+  }
+});
+
+function printMessage(channel, message)
+{
+  console.log('\nTECHIO> message --channel "' + channel + '" "' + message + '"');
+}
