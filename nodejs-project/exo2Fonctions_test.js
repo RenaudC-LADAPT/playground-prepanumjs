34a4da7302e88098b61e7fffd9347d171aa9b910
RenaudC-LADAPT <72964373+RenaudC-LADAPT@users.noreply.github.com>
2022-12-13 09:59:00 +0100

    Create exo2Fonctions_test.js

---
+++ nodejs-project/exo2Fonctions_test.js
@@ -0,0 +1,21 @@
+var assert = require('assert');
+var mod = require('./exo2Fonctions.js');
+
+it("Concersion Francs suisses/Euros", function () {
+  try
+  {
+    assert.equal( typeof(mod.chf), "number" );
+    assert.equal( typeof(mod.euros), "number" );
+    assert.equal( mod.chf
+  }
+  catch (error)
+  {
+    printMessage('Attention', 'revoyez le contenu de votre fonction"');
+    throw error;
+  }
+});
+
+function printMessage(channel, message)
+{
+  console.log('\nTECHIO> message --channel "' + channel + '" "' + message + '"');
+}
