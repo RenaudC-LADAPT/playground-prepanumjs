ce4ae6763c477a48747942393517ff85c5f290bd
RenaudC-LADAPT <72964373+RenaudC-LADAPT@users.noreply.github.com>
2022-12-13 11:13:12 +0100

    Update exo3Fonctions.js

--- nodejs-project/exo3Fonctions.js
+++ nodejs-project/exo3Fonctions.js
@@ -1,1 +1,13 @@
+//compléter le code ici afin de déclarer  une fonction nommée minuteToSecond qui renvoi les minutes converties en secondes.
 
+
+
+
+// { autofold
+let test1 = minuteToSecond(4);
+let test2 = minuteToSecond(3);
+module.exports = {
+	test2: test2,
+	test1: test1
+};
+// }
