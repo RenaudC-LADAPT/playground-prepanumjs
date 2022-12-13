26eaa84eb02a145b4fa72e7463fe33ad70060a9b
RenaudC-LADAPT <72964373+RenaudC-LADAPT@users.noreply.github.com>
2022-12-13 11:55:04 +0100

    Create exo5Fonctions.js

---
+++ nodejs-project/exo5Fonctions.js
@@ -0,0 +1,21 @@
+//compléter le code ici afin de déclarer une fonction nommée nbrsOeufs.
+
+
+
+
+// { autofold
+test1 = nbrsOeufs(1, 0, 0, 0);
+test2 = nbrsOeufs(0, 1, 0, 0);
+test3 = nbrsOeufs(0, 0, 1, 0);
+test3b = nbrsOeufs(1, 1, 0, 0);
+test4 = nbrsOeufs(0, 0, 0, 1);
+test4b = nbrsOeufs(0, 2, 0, 0);
+module.exports = {
+	test1: test1,
+	test2: test2,
+	test3: test3,
+	test3b: test3b,
+	test4: test4,
+	test4b: test4b,
+};
+// }
