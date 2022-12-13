f9003c20b285564a983e75e961764e3586b4029c
RenaudC-LADAPT <72964373+RenaudC-LADAPT@users.noreply.github.com>
2022-12-13 09:53:35 +0100

    Update exo2Fonctions.js

--- nodejs-project/exo2Fonctions.js
+++ nodejs-project/exo2Fonctions.js
@@ -1,7 +1,7 @@
 let chf = 10;
 
 function francs_suisses_en_euros(x) {
- //compléter le code ici afin que la fonction renvoi en la somme en franc suisse ebtrée en parametre, convertie en euros
+ //compléter le code ici afin que la fonction renvoi en la somme x en franc suisse, convertie en euros
  // indice 1 CHF = 1.01367 EUR
 }
 
@@ -10,6 +10,6 @@ let euros = francs_suisses_en_euros(chf)
 // { autofold
 module.exports = {
 	chf: chf,
-  euros: chf * 1.01367,
+	euros: chf * 1.01367,
 };
 // }
