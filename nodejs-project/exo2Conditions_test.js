6229ef29f67e44904e7bd86456544637fc2c586e
RenaudC-LADAPT <72964373+RenaudC-LADAPT@users.noreply.github.com>
2023-01-02 14:34:14 +0100

    Create exo2Conditions_test.js

---
+++ nodejs-project/exo2Conditions_test.js
@@ -0,0 +1,23 @@
+
+var assert = require('assert');
+var mod = require('./exo1Conditions.js');
+
+it("Est Pair ?", function () {
+  try
+  {
+    assert.equal( typeof(mod.test1), "boolean" );
+    assert.equal( mod.test1, false );
+    assert.equal( typeof(mod.test2), "boolean" );
+    assert.equal( mod.test2, true );
+  }
+  catch (error)
+  {
+    printMessage('Attention', 'revoyez la déclaration de votre fonction majorite');
+    throw error;
+  }
+});
+
+function printMessage(channel, message)
+{
+  console.log('\nTECHIO> message --channel "' + channel + '" "' + message + '"');
+}
