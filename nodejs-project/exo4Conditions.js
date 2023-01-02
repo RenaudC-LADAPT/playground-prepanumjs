9c5c1fa427cdc0c4c92a245ffbd182c4908424b8
RenaudC-LADAPT <72964373+RenaudC-LADAPT@users.noreply.github.com>
2023-01-02 16:03:49 +0100

    Update exo4Conditions.js

--- nodejs-project/exo4Conditions.js
+++ nodejs-project/exo4Conditions.js
@@ -1,1 +1,2 @@
-
+//Écrivez une fonction maFunction qui prend un nombre compris entre -100 et 100, et renvoie un message selon que le nombre est positif, négatif ou nul.
+//Si le nombre est en dehors de l'intervalle autorisé, la fonction renvoie un message d'erreur.
