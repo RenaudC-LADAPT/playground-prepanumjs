6e3861e7d09edad9c2a2d9dfdc9edd1c16425bc6
RenaudC-LADAPT <72964373+RenaudC-LADAPT@users.noreply.github.com>
2023-01-02 15:05:49 +0100

    Update exo3Conditions.js

--- nodejs-project/exo3Conditions.js
+++ nodejs-project/exo3Conditions.js
@@ -1,1 +1,13 @@
+//Écrivez une fonction div qui prend deux nombres en paramètres, et renvoi un message qui donne le résultat de leur division. "Le résultat de la division est ... ."
+//Si le deuxième nombre est nul, la fonction doit renvoyer un message d'erreur : "Erreur : division par zéro interdite." .
 
+  
+
+// { autofold
+test1 = div(42, 6);
+test2 = div(42, 0);
+module.exports = {
+	test1: test1,
+	test2: test2,
+};
+// }
