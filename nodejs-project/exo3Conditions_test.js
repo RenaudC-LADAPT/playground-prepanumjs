4a31120e0dcaf6b6a0bc3a0b05a8988c8a4e987a
RenaudC-LADAPT <72964373+RenaudC-LADAPT@users.noreply.github.com>
2023-01-02 16:08:50 +0100

    Update exo3Conditions_test.js

--- nodejs-project/exo3Conditions_test.js
+++ nodejs-project/exo3Conditions_test.js
@@ -1,1 +1,23 @@
 
+var assert = require('assert');
+var mod = require('./exo3Conditions.js');
+
+it("Est Pair ?", function () {
+  try
+  {
+    assert.equal( typeof(mod.test1), "string" );
+    assert.equal( mod.test1, "Le résultat de la division est 7." );
+    assert.equal( typeof(mod.test2), "string" );
+    assert.equal( mod.test2, "Erreur : division par zéro interdite." );
+  }
+  catch (error)
+  {
+    printMessage('Attention', 'revoyez la déclaration de votre fonction majorite');
+    throw error;
+  }
+});
+
+function printMessage(channel, message)
+{
+  console.log('\nTECHIO> message --channel "' + channel + '" "' + message + '"');
+}
