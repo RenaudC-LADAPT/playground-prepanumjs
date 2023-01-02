b68ae4e81a5de49ebc9169ead657e4d66f2fd543
RenaudC-LADAPT <72964373+RenaudC-LADAPT@users.noreply.github.com>
2023-01-02 16:01:31 +0100

    Update exo2Conditions.js

--- nodejs-project/exo2Conditions.js
+++ nodejs-project/exo2Conditions.js
@@ -1,7 +1,7 @@
 //Écrivez une fonction `majorite` qui prend un âge (nombre entier) et retourne un message différent selon que l'utilisateur est majeur ou mineur.
 
   
-};
+
 // { autofold
 test1 = estPair(3);
 test2 = estPair(42);
