af65a5e991bcae8ce3b84811a1761ec2e5bf37d2
RenaudC-LADAPT <72964373+RenaudC-LADAPT@users.noreply.github.com>
2023-01-02 16:02:10 +0100

    Update exo2Conditions.js

--- nodejs-project/exo2Conditions.js
+++ nodejs-project/exo2Conditions.js
@@ -1,4 +1,4 @@
-//Écrivez une fonction `majorite` qui prend un âge (nombre entier) et retourne un message différent selon que l'utilisateur est majeur ou mineur.
+//Écrivez une fonction estPair qui prend en paramètre un nombre entier et qui retourne true si le nombre est pair, et false s'il est impair.
 
   
 
