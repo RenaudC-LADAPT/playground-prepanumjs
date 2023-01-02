8eaacd9561dc6f2c2cf5934dbbf7f0a284e7cc9c
RenaudC-LADAPT <72964373+RenaudC-LADAPT@users.noreply.github.com>
2023-01-02 16:13:07 +0100

    Update exo4Conditions.js

--- nodejs-project/exo4Conditions.js
+++ nodejs-project/exo4Conditions.js
@@ -1,2 +1,19 @@
-//Écrivez une fonction maFunction qui prend un nombre compris entre -100 et 100, et renvoie un message selon que le nombre est positif, négatif ou nul.
-//Si le nombre est en dehors de l'intervalle autorisé, la fonction renvoie un message d'erreur.
+//Écrivez une fonction maFunction qui prend un nombre compris entre -100 et 100, et renvoie un message selon que le nombre est positif, négatif ou nul : 
+//  "Le nombre est positif." ou "Le nombre est négatif." ou "Le nombre est nul."
+//Si le nombre est en dehors de l'intervalle autorisé, la fonction renvoie un message d'erreur :
+//  "Erreur : le nombre est en dehors de l'intervalle autorisé."
+
+
+
+// { autofold
+let test1 = maFunction(4);
+let test2 = maFunction(-3);
+let test3 = maFunction(0);
+let test4 = maFunction(103);
+module.exports = {
+	test1: test1,
+	test2: test2,
+  test3: test3,
+	test4: test4,
+};
+// }
