0fe318ea376bf4af13ec542ad1191d06ab8f018f
RenaudC-LADAPT <72964373+RenaudC-LADAPT@users.noreply.github.com>
2023-01-02 11:17:05 +0100

    Create exo1Conditions.js

---
+++ nodejs-project/exo1Conditions.js
@@ -0,0 +1,12 @@
+//Écrivez une fonction `majorite` qui prend un âge (nombre entier) et retourne un message différent selon que l'utilisateur est majeur ou mineur.
+function majorite(age) {
+  
+};
+// { autofold
+test1 = majorite(3);
+test2 = majorite(42);
+module.exports = {
+	test1: test1,
+	test2: test2,
+};
+// }
