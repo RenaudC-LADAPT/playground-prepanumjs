6138149c40937c4119e341c65cb49869ebe1281a
RenaudC-LADAPT <72964373+RenaudC-LADAPT@users.noreply.github.com>
2023-01-09 15:36:27 +0100

    Update exo5Conditions.js

--- nodejs-project/exo5Conditions.js
+++ nodejs-project/exo5Conditions.js
@@ -3,11 +3,15 @@
 
 // { autofold
 let test1 = estBissextile(1999);
-let test2 = estBissextile(2000);
+let test2 = estBissextile(1900);
 let test3 = estBissextile(2004);
+let test4 = estBissextile(2000);
+let test5 = estBissextile(1582);
 module.exports = {
 	test1: test1,
 	test2: test2,
-  test3: test3,
+	test3: test3,
+	test4: test4,
+	test5: test5,
 };
 // }
