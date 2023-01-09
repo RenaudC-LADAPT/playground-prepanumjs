6b17e7f9b2b4ee87364951c50e582d6a5ee95ffe
RenaudC-LADAPT <72964373+RenaudC-LADAPT@users.noreply.github.com>
2023-01-09 15:22:20 +0100

    Create exo5Conditions.js

---
+++ nodejs-project/exo5Conditions.js
@@ -0,0 +1,13 @@
+
+
+
+// { autofold
+let test1 = estBissextile(1999);
+let test2 = estBissextile(2000);
+let test3 = estBissextile(2004);
+module.exports = {
+	test1: test1,
+	test2: test2,
+  test3: test3,
+};
+// }
