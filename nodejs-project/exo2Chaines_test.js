9c1e8bde679dafa6ee375845e69befcbcb909b99
RenaudC-LADAPT <72964373+RenaudC-LADAPT@users.noreply.github.com>
2023-01-10 15:40:56 +0100

    Create exo2Chaines_test.js

---
+++ nodejs-project/exo2Chaines_test.js
@@ -0,0 +1,22 @@
+var assert = require('assert');
+var mod = require('./exo2Chaines.js');
+
+it("Nombres de Voyelles", function () {
+  try
+  {
+    assert.equal( typeof(mod.test1), "number" );
+    assert.equal( mod.test1, 3 );
+    assert.equal( typeof(mod.test2), "number" );
+     assert.equal( mod.test2, 3 );
+  }
+  catch (error)
+  {
+    printMessage('Attention', 'revoyez la déclaration de votre fonction minuteToSecond');
+    throw error;
+  }
+});
+
+function printMessage(channel, message)
+{
+  console.log('\nTECHIO> message --channel "' + channel + '" "' + message + '"');
+}
