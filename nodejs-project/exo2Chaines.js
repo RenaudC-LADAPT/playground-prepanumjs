d7cd4bd6a9740cae668df58350e93166055201ad
RenaudC-LADAPT <72964373+RenaudC-LADAPT@users.noreply.github.com>
2023-01-10 15:37:15 +0100

    Create exo2Chaines.js

---
+++ nodejs-project/exo2Chaines.js
@@ -0,0 +1,11 @@
+//Écrivez une fonction vowelCount(str) qui prend en paramètre une chaîne de caractères et qui retourne le nombre de voyelles contenues dans la chaîne.
+
+
+// { autofold
+let test1 = vowelCount("Hello World!");
+let test2 = vowelCount("javascript");
+module.exports = {
+	test1: test1,
+	test2: test2,
+};
+// }
