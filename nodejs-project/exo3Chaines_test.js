41e428eb92509ab5530ac4a1aa99aa63be5433a5
RenaudC-LADAPT <72964373+RenaudC-LADAPT@users.noreply.github.com>
2023-01-11 11:17:43 +0100

    Update exo3Chaines_test.js

--- nodejs-project/exo3Chaines_test.js
+++ nodejs-project/exo3Chaines_test.js
@@ -1,1 +1,20 @@
+var assert = require('assert');
+var mod = require('./exo3Chaines.js');
 
+it("Maitre Yoda", function () {
+  try
+  {
+    assert.equal( typeof(mod.test1), "string" );
+    assert.equal( mod.test1,  "Hell0 W0rld!" );
+  }
+  catch (error)
+  {
+    printMessage('Attention', 'revoyez la déclaration de votre fonction majorite');
+    throw error;
+  }
+});
+
+function printMessage(channel, message)
+{
+  console.log('\nTECHIO> message --channel "' + channel + '" "' + message + '"');
+}
