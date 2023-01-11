c2c7ff0ce622f5c34e86434d8ee5907f57285785
RenaudC-LADAPT <72964373+RenaudC-LADAPT@users.noreply.github.com>
2023-01-11 11:17:07 +0100

    Update exo3Chaines.js

--- nodejs-project/exo3Chaines.js
+++ nodejs-project/exo3Chaines.js
@@ -1,1 +1,9 @@
+//replace(phrase, aRemplacer, remplacant) renvoie la phrase avec le caractère aRemplacer remplacé par le caractère remplacant
 
+
+// { autofold
+replace("Hello World!", "o", "0");
+module.exports = {
+	test1: test1,
+};
+// }
