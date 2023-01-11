550fa32829d6e16692b71b29457dbccf7a6fd0b7
RenaudC-LADAPT <72964373+RenaudC-LADAPT@users.noreply.github.com>
2023-01-11 11:06:16 +0100

    Create exo2Chaines.js

---
+++ nodejs-project/exo2Chaines.js
@@ -0,0 +1,11 @@
+//Écrivez une fonction verificationMotDePasse(password) qui prend un argument de type string.
+//Elle devra retourner un boolean qui vaut true si le password fait au moins 12 caractères et false si moins.
+
+
+// { autofold
+test1 = verificationMotDePasse('password');
+test2 = verificationMotDePasse('motdepasselong')
+module.exports = {
+	test1: test1,
+};
+// }
