a06554699510b40fb64b3467f98f0f0a092bc8b9
RenaudC-LADAPT <72964373+RenaudC-LADAPT@users.noreply.github.com>
2023-01-11 11:00:39 +0100

    Create exo1Chaines_test.js

---
+++ nodejs-project/exo1Chaines_test.js
@@ -0,0 +1,20 @@
+var assert = require('assert');
+var mod = require('./exo1Chaines.js');
+
+it("Maitre Yoda", function () {
+  try
+  {
+    assert.equal( typeof(mod.test1), "string" );
+    assert.equal( mod.test1, ""Bonjour Suzanne, que la Force soit avec toi"" );
+  }
+  catch (error)
+  {
+    printMessage('Attention', 'revoyez la déclaration de votre fonction majorite');
+    throw error;
+  }
+});
+
+function printMessage(channel, message)
+{
+  console.log('\nTECHIO> message --channel "' + channel + '" "' + message + '"');
+}
