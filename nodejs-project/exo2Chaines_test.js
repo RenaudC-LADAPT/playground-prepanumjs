a727a68ad737677205ef14901a81a5724dc954b3
RenaudC-LADAPT <72964373+RenaudC-LADAPT@users.noreply.github.com>
2023-01-11 11:10:21 +0100

    Create exo2Chaines_test.js

---
+++ nodejs-project/exo2Chaines_test.js
@@ -0,0 +1,22 @@
+var assert = require('assert');
+var mod = require('./exo1Chaines.js');
+
+it("Mot de passe", function () {
+  try
+  {
+    assert.equal( typeof(mod.test1), "boolean" );
+    assert.equal( mod.test1, false );
+    assert.equal( typeof(mod.test2), "boolean" );
+    assert.equal( mod.test1, true );
+  }
+  catch (error)
+  {
+    printMessage('Attention', 'revoyez la déclaration de votre fonction majorite');
+    throw error;
+  }
+});
+
+function printMessage(channel, message)
+{
+  console.log('\nTECHIO> message --channel "' + channel + '" "' + message + '"');
+}
