330f19a732799cba0880c9fda0954e8765d7d8c8
RenaudC-LADAPT <72964373+RenaudC-LADAPT@users.noreply.github.com>
2023-01-11 11:19:49 +0100

    Create exo4Chaines.js

---
+++ nodejs-project/exo4Chaines.js
@@ -0,0 +1,11 @@
+//Écrivez une fonction lettreEnChiffre(str) qui prend un argument de type string.
+//Elle devra retourner cette même string mais en remplacant les e (ou E) par des 3, les i ou I) par des 1 et les o (ou O) par des 0 et les (t ou T) par des 7 :
+
+
+
+// { autofold
+test1 = lettreEnChiffre("LADAPT | L’association pour l’insertion sociale et professionnelle des personnes handicapées");
+module.exports = {
+	test1: test1,
+};
+// }
