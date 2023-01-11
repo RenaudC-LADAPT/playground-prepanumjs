4c9fbca75d0f9e55c7dbe0f508c0e3f4301dba29
RenaudC-LADAPT <72964373+RenaudC-LADAPT@users.noreply.github.com>
2023-01-11 10:59:12 +0100

    Create exo1Chaines.js

---
+++ nodejs-project/exo1Chaines.js
@@ -0,0 +1,9 @@
+//Écrivez une fonction bonjour() qui prend en paramètre un Prénom sous forme de chaîne de caractères et qui retourne "Bonjour 'Prenom', que la Force soit avec toi".
+
+
+// { autofold
+test1 = bonjour("Suzanne");
+module.exports = {
+	test1: test1,
+};
+// }
