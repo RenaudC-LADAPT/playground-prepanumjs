951a1747a2ea60aa1d4ef95eb67034360ad0c500
RenaudC-LADAPT <72964373+RenaudC-LADAPT@users.noreply.github.com>
2023-01-12 11:10:19 +0100

    Update exo3Chaines_test.js

--- nodejs-project/exo3Chaines_test.js
+++ nodejs-project/exo3Chaines_test.js
@@ -1,11 +1,17 @@
 var assert = require('assert');
 var mod = require('./exo3Chaines.js');
 
-it("Remplacement", function () {
+it("Chaînes de caractères et index", function () {
   try
   {
     assert.equal( typeof(mod.test1), "string" );
-    assert.equal( mod.test1,  "Hell0 W0rld!" );
+    assert.equal( mod.test1,  "Bonjour tout le monde, comment allez-vous aujourd'hui?" );
+    assert.equal( typeof(mod.test2), "string" );
+    assert.equal( mod.test2,  "n" );
+    assert.equal( typeof(mod.test3), "string" );
+    assert.equal( mod.test3,  "out le mond" );
+    assert.equal( typeof(mod.test4), "number" );
+    assert.equal( mod.test4, 31 );
   }
   catch (error)
   {
