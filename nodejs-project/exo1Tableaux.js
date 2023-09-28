a5cf4ddd4f54da5f9030625b817b4c5f1bb5764c
RenaudC-LADAPT <72964373+RenaudC-LADAPT@users.noreply.github.com>
2023-09-28 11:27:46 +0200

    Create exo1Tableaux.js

---
+++ nodejs-project/exo1Tableaux.js
@@ -0,0 +1,13 @@
+let rubriques = ['Accessibilité', 'Mentions légales', 'Crédits', 'Rejoignez-nous', 'Publications', 'Presse',  'Contact']
+
+//Remplacez 'Presse' parle 'On parle de nous' dans le tableau rubriques
+// Vous ne devez pas modifierla ligne 1 de ce code ecrire du code ci dessous pour arrivber à vos fins
+
+
+
+// { autofold
+console.log(`le 6e élément du tableau est désormais ${rubriques[5]}`);
+module.exports = {
+	test: rubriques[5],
+};
+// }
